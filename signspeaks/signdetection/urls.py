70a3b9eedff83289c4311a13fcb8eb01ea79e163
pratik-rgp1 <pratikregmi989@gmail.com>
2024-04-30 22:36:03 +0545

    login page created

--- signspeaks/signdetection/urls.py
+++ signspeaks/signdetection/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("",views.homepage, name="homepage"),
     path("aboutus/",views.aboutus, name="aboutus"),
     path("contactus/",views.contactus, name="contactus"),
-    path("registerpage/",views.register, name="registerpage")
-    
+    path("registerpage",views.register, name="registerpage"),
+    path("login",views.login, name="login")
 ]
--- signspeaks/signdetection/views.py
+++ signspeaks/signdetection/views.py
@@ -20,9 +20,8 @@ def register(request):
         form = NewUserForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)
             messages.success(request, "Registration successful." )
-            return redirect("signspeaks:homepage")
+            return redirect("homepage")
         else:
             print(form.errors)  # Print form errors to console for debugging
             messages.error(request, "Unsuccessful registration. Please correct the errors.")
@@ -30,6 +29,24 @@ def register(request):
         form = NewUserForm()
     return render(request=request, template_name="user/register.html", context={"register_form":form})
 
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                # login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("homepage")
+            else:
+                messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="user/login.html", context={"login_form":form})
+	
 
 def homepage(request):
     return render(request,'homepage.html')
